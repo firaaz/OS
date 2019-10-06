dd5af56cd627ca01e6cae133eef4702704ab7c3c
Firaaz <firaazfarook19@gmail.com>
2019-10-07 01:19:26 +0530

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
 #![no_std]                  // Rust standard library is not linked
 #![no_main]                 // disable the main() entry point
 
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
+
 mod vga_buffer;
+mod serial;
 
 // Function called on panic
 #[panic_handler]
@@ -17,5 +24,42 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    println!("trivial assertion ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
