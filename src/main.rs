6bbba78fc7cd808a98c2bb8647beb09e96a18e2a
Firaaz <firaazfarook19@gmail.com>
2019-10-08 02:23:24 +0530

    integration tests and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os::test_runner)]
 
 #![no_std]                  // Rust standard library is not linked
 #![no_main]                 // disable the main() entry point
@@ -7,26 +7,23 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+// use os::println;
 
 mod vga_buffer;
 mod serial;
 
-// removed as prints on the console
-/* #[cfg(not(test))]
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop{}
-} */
+}
 
 // Function called on panic
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop{}
+    os::test_panic_handler(info);
 }
 
 // disable the mangling of the name so that is can be called by the linker as
@@ -39,38 +36,4 @@ pub extern "C" fn _start() {
     test_main();
 
     loop {}
-}
-
-
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion........");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -148,3 +148,27 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
+#[test_case]
+fn test_println_simple() {
+    serial_print!("test_println");
+    println!("test println simple output");
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_output() {
+    serial_print!("test_println_output.....");
+
+    let s = "Some test th";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+
+    serial_println!("[ok]");
+}
